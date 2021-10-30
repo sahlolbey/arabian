f5586cd1d732e73bca3e702045961c66d2d0e676
hamid <sahlolbey@gmail.com>
2021-10-30 23:42:31 +0330

    edited

--- src/test/java/com/fsp/arabian/AccountServiceTest.java
+++ src/test/java/com/fsp/arabian/AccountServiceTest.java
@@ -1,12 +1,8 @@
 package com.fsp.arabian;
 
-import com.anarsoft.vmlens.concurrent.junit.ConcurrentTestRunner;
 import com.fsp.arabian.model.Account;
 import com.fsp.arabian.service.AccountService;
-import org.h2.jdbc.JdbcSQLTransactionRollbackException;
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +14,6 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @Sql (executionPhase= Sql.ExecutionPhase.BEFORE_TEST_METHOD,
         scripts = {"/data.sql"})
@@ -33,39 +28,50 @@ public class AccountServiceTest {
 
 
     @Test
-    public void loadData(){
+    public void testGetAccount(){
         log.info ("running test");
         Account account = new Account (1234L);
-        if(accountService == null)
-            log.info ("accountservice is null");
         Optional<Account> accountOpt =
                 accountService.getAccount (account);
         if(accountOpt.isPresent ()){
-            System.out.println ("Balance = " +accountOpt.get ().getBalance ());
+            assertEquals (10000.25,accountOpt.get().getBalance ());
 
         }
     }
     @Test
-    public void testTransfer(){
+    public void testTransferInvalidAccounts(){
+        Account source = new Account(654321L);
+        Account dest = new Account (1234L);
+        int result = accountService.transfer ("1234",source,dest,1000.25);
+        assertEquals (-1,result);
+    }
+    @Test
+    public void testTransferNotEnoughBalance(){
+        Account a1234 = new Account (1234L);
+        Account a1235 = new Account (1235L);
+        int result=accountService.transfer ("1234" , a1234 ,
+                a1235 , 11000.00);
+        assertEquals (0,result);
+        a1234 = accountService.getAccount (a1234).get ();
+        a1235 = accountService.getAccount (a1235).get ();
+        assertEquals (10000.25,a1234.getBalance ());
+        assertEquals (11000.75,a1235.getBalance ());
+    }
+    @Test
+    public void testTransferMultiThread(){
 
         Account a1234 = new Account (1234L);
         Account a1235 = new Account (1235L);
-        Account a1236 = new Account (1236L);
-        Account a1237 = new Account (1237L);
-        Account a1238 = new Account (1238L);
-        Account a1239 = new Account (1239L);
 
         TransferTest test1 = new TransferTest (a1234,a1235,1000.00);
-        TransferTest test2 = new TransferTest (a1237,a1235,1000.00);
-        TransferTest test3 = new TransferTest (a1235,a1234,1000.00);
+
+        TransferTest test2 = new TransferTest (a1235,a1234,1000.00);
 
         Thread t1 = new Thread (test1);
         Thread t2 = new Thread (test2);
-        Thread t3 = new Thread (test3);
         t1.start ();
         t2.start ();
-        t3.start ();
-        while (t1.isAlive () && t2.isAlive () &&  t3.isAlive ()){
+        while (t1.isAlive () && t2.isAlive ()){
             System.out.println ("waiting for all to return");
             try {
                 Thread.sleep (1000);
@@ -79,17 +85,7 @@ public class AccountServiceTest {
         System.out.println ("a1234 balance");
         assertEquals (10000.25,a1234.getBalance ());
         System.out.println ("a1235 balance");
-        assertEquals (12000.75,a1235.getBalance ());
-
-        try {
-            Thread.sleep (3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace ();
-        }
-
-
-     //   t3.start ();
-    //    t2.start ();
+        assertEquals (11000.75,a1235.getBalance ());
 
     }
     class TransferTest implements Runnable{
@@ -114,9 +110,7 @@ public class AccountServiceTest {
                             destination , amount);
                      if(result == 1) done = true;
                 } catch (Exception e) {
-                    log.warn (e.getClass ().toString ());
                     log.warn ("failed "+(count++));
-
                 }
             }
         }
