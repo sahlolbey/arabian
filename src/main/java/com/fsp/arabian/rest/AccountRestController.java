cda2edd8dd83940fd3fc2054f2b14502bf7234c9
hamid <sahlolbey@gmail.com>
2021-10-30 23:42:09 +0330

    edited

--- src/main/java/com/fsp/arabian/rest/AccountRestController.java
+++ src/main/java/com/fsp/arabian/rest/AccountRestController.java
@@ -1,11 +1,12 @@
 package com.fsp.arabian.rest;
 
 import com.fsp.arabian.model.Account;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountRestController {
-
+    @PostMapping("/transactions")
     public Integer transfer(Account source,
                             Account destination,Double amount){
         return null;
