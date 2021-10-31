debdce9a34505ffaac3f7dfece9fa640d786416c
hamid <sahlolbey@gmail.com>
2021-10-31 04:31:25 +0330

    edited

--- src/test/java/com/fsp/arabian/AccountRestControllerTest.java
+++ src/test/java/com/fsp/arabian/AccountRestControllerTest.java
@@ -1,29 +1,12 @@
 package com.fsp.arabian;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fsp.arabian.model.Account;
-import org.hibernate.engine.jdbc.env.spi.IdentifierCaseStrategy;
 import org.junit.jupiter.api.Test;
-
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.*;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class AccountRestControllerTest {
