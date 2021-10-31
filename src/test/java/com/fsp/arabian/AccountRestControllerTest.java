7a6a2e5ea458ad75809d779b5cd82bc6595321a9
hamid <sahlolbey@gmail.com>
2021-10-31 04:34:25 +0330

    edited

--- src/test/java/com/fsp/arabian/AccountRestControllerTest.java
+++ src/test/java/com/fsp/arabian/AccountRestControllerTest.java
@@ -2,17 +2,12 @@ package com.fsp.arabian;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootTest
 public class AccountRestControllerTest {
     @Test
     public void testTransfer(){
-    
+
     }
 
 }
